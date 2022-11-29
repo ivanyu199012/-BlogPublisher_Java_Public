da0d92cb1f8653efef9c0a67f6c4974491692e48
ivanyu199012 <chuenyuen027546@gmail.com>
2022-11-29 16:21:55 +0900

    Init Project

---
+++ src/main/java/com/blog/publish/publisher/App.java
@@ -0,0 +1,13 @@
+package com.blog.publish.publisher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Util.printMessage("Hello World");
+    }
+}
---
+++ src/main/java/com/blog/publish/publisher/Util.java
@@ -0,0 +1,9 @@
+package com.blog.publish.publisher;
+
+public class Util 
+{
+	public static void printMessage(String message)
+	{
+		System.out.println(message);
+	}
+}
