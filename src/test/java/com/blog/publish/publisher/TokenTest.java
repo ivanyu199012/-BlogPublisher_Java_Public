3e71cf5071e0632bc57de5046cd9293c84a481fc
ivanyu@xerox <chuenyuen027546@gmail.com>
2022-11-30 14:35:55 +0900

    log4j 2 추가

--- src/test/java/com/blog/publish/publisher/TokenTest.java
+++ src/test/java/com/blog/publish/publisher/TokenTest.java
@@ -2,16 +2,21 @@ package com.blog.publish.publisher;
 
 import static org.junit.Assert.assertTrue;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
 import com.blog.publish.publisher.utils.Token;
 
 public class TokenTest {
 	
+	private static Logger logger = LogManager.getLogger(TokenTest.class);
+	
 	@Test
 	public void test_getGithubToken()
 	{
 		String token = Token.getGithubToken();
+		logger.info("token = " + token);
 		assertTrue(token.contains("ghp_"));
 	}
 }
